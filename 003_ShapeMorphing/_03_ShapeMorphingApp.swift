050bedbbd5a54a24da57c77c09cd4c700a4b60b6
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-09-19 23:51:06 +0300

    Initial Commit

---
+++ 003_ShapeMorphing/_03_ShapeMorphingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _03_ShapeMorphingApp.swift
+//  003_ShapeMorphing
+//
+//  Created by nikita on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _03_ShapeMorphingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
